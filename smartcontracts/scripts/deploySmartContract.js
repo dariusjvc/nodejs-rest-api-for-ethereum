e86dbb62d5364eef08e31bfa07da4892ba49619b
Darío <djvalarezo@minsait.com>
2023-04-09 13:56:48 +0200

    Deleted not used variable

--- smartcontracts/scripts/deploySmartContract.js
+++ smartcontracts/scripts/deploySmartContract.js
@@ -1,5 +1,5 @@
 async function main() {
-    const [deployer, bank1, bank2, bank3] = await ethers.getSigners();
+    const [deployer] = await ethers.getSigners();
 
     console.log("Deploying contracts with the account:", deployer.address);
 
@@ -7,14 +7,8 @@ async function main() {
 
     const UpgrableTokenTest = await ethers.getContractFactory("UpgradableTokenTest");
     const token = await upgrades.deployProxy(UpgrableTokenTest, ["TestToken", "SPM", 2, 100000]);
-    /*await tokenUpgradebale.createBank("Bank1", bank1.address);
-    await tokenUpgradebale.createBank("Bank2", bank2.address);
-    await tokenUpgradebale.createBank("Bank3", bank3.address);*/
 
     console.log("Token address:", token.address);
-    /*console.log("Bank1 address:", bank1.address);
-    console.log("Bank2 address:", bank2.address);
-    console.log("Bank3 address:", bank3.address);*/
 }
 
 main()
